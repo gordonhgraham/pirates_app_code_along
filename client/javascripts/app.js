5e561a8f12b269991dcd360a9d161bb032c22e82
Craig Quincy <craig.quincy@galvanize.com>
2016-11-02 11:32:39 -0600

    remove new functionality

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -14,19 +14,6 @@
           pirates: getAllPirates
         }
       })
-      .when('/pirates/new', {
-        templateUrl: '../views/pirates/new.html',
-        controller: 'NewPirateController',
-        controllerAs: 'vm'
-      })
-      // .when('/pirates/:id/edit', {
-      //   templateUrl: '../views/pirates/edit.html',
-      //   controller: 'EditPirateController',
-      //   controllerAs: 'vm',
-      //   resolve: {
-      //     pirate: getPirateById
-      //   }
-      // })
       .otherwise({redirectTo: '/pirates'})
     $locationProvider.html5Mode(true);
   }
@@ -35,12 +22,7 @@
     return PirateService.getPirates();
   }
 
-  // function getPirateById(PirateService, $route) {
-  //   return PirateService.getPirate($route.current.params.id);
-  // }
-
   config.$inject = ['$routeProvider', '$locationProvider'];
   getAllPirates.$inject = ['PirateService'];
-  // getPirateById.$inject = ['PirateService', '$route'];
 
 })()
--- client/javascripts/services.js
+++ client/javascripts/services.js
@@ -11,21 +11,6 @@
         return $http.get(BASE_URL);
       }
 
-      this.createPirate = function(newPirate) {
-        return $http.post(BASE_URL, newPirate);
-      }
-
-      // this.getPirate = function(id) {
-      //   return $http.get(BASE_URL + "/" + id);
-      // }
-      //
-      // this.deletePirate = function(id) {
-      //   return $http.delete(BASE_URL + "/" + id);
-      // }
-      //
-      // this.updatePirate = function(updatedPirate) {
-      //   return $http.put(BASE_URL + "/" + updatedPirate.id, updatedPirate);
-      // }
     }
 
     PirateService.$inject = ["$http"];
